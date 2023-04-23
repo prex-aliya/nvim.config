9a8eca2a3def23b9a2f65ce2f5670dd541e7ddbf
prex-aliya <droved-through@proton.me>
2023-04-23 02:35:15 -0400

    remove moonfly from init

--- init.lua
+++ init.lua
@@ -30,7 +30,6 @@ local config_files = vim.fn.stdpath('config') .. '/?.lua'
 package.path = package.path .. ';' .. config_files
 
 
---require('moonfly')
 require('lua/map') -- nvim keymaps         "map()"
 require('lua/opt') -- vim normal set       "vim.opt"
 require('lua/plug') -- nvim plugins and configurations
